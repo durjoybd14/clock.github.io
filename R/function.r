83b75739f7bf72fc16a5c8a086ec2d84b728e319
durjoybd14 <nrd141913@gmail.com>
2023-12-03 12:28:58 +0600

    clock 1st

---
+++ R/function.r
@@ -0,0 +1,21 @@
+# myFunction <- function(a, b) {
+#     print(a + b)
+# }
+
+# myFunction(1, 2)
+
+# sumFunction <- function(a, b) {
+#     sum <- a + b
+#     return(sum)
+# }
+
+
+# print(sumFunction(2, 5))
+
+sumFunction <- function(a, b) {
+    sum <- a + b
+    return(sum)
+}
+
+printSum <- sumFunction(2, 5)
+print(printSum)
